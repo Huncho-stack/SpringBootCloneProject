c5d436fdb328af2a4cacdd65da8b4056431aec7a
OkorieIfeanyiKingsley <okorieifeanyi257@gmail.com>
2022-08-10 12:57:52 +0100

    Added user controller

---
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -0,0 +1,21 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+	
+	public String home() {
+		
+		return "Welcome To HOME PAGE";
+	}
+	
+	
+	public String dashboard() {
+		
+		return "Welcome To DASHBOARD PAGE";
+	}
+
+}
